a97b0665e369e0b0266e9a0d34b51bb5c2004a04
jopinta <tatinfa@yahoo.com>
2021-06-22 14:03:49 -0700

    no print just return

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_abs - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *@j: - nada nada
+*/
+
+
+int _abs(int j)
+{
+
+if (j > 0)
+{
+return (j);
+}
+else
+{
+return (-j);
+}
+}
