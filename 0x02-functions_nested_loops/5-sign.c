c49f2ebf529655280d6178ba9abf9ccee327b3fd
jopinta <tatinfa@yahoo.com>
2021-06-22 13:10:03 -0700

    porque imprime dos + y la barra?

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_sign - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *@n: sign
+*/
+
+int print_sign(int n)
+{
+
+
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+ else
+{
+_putchar('-');
+return (-1);
+}
+}
