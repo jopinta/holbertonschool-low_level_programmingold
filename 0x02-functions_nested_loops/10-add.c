85e552e85276ebd28989d5c40e5c9cf8cdadc81b
jopinta <tatinfa@yahoo.com>
2021-06-22 19:06:59 -0700

    hacerle +=

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *@a: sumador
+ *@b:sumando
+*/
+
+int add(int a, int b)
+{
+
+a += b;
+return (a);
+}
