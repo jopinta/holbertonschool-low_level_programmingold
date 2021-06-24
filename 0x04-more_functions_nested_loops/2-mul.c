2fe7b59e26d229df993722992a82b8acb90f4682
jopinta <tatinfa@yahoo.com>
2021-06-24 16:00:06 -0700

    subir el .c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *@a: aa
+ *@b: bb
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
