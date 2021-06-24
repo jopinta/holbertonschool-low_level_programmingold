ba29daf4ce9a1daa6569689ada760d3934b27b78
jopinta <tatinfa@yahoo.com>
2021-06-24 16:58:44 -0700

    los queno van no es && es || carai

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+if (i == '2' || i == '4')
+{
+continue;
+}
+_putchar(i);
+}
+_putchar('\n');
+}
