c413b2c72ee4a7aadfdcd24af6a8c791729c40f9
jopinta <tatinfa@yahoo.com>
2021-06-25 16:28:41 -0700

    mmmm

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+
+/*
+* print_something - function to print something
+* times_table - (void)
+* Return: Always 0
+**/
+
+void times_table(void)
+{
+
+int i, j,num = 9;
+for (i = 0; i < num + 1; i++)
+{
+for (j = 0; j < num + 1; j++)
+{
+printf("%d", i * j);
+if (j < num)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+}
