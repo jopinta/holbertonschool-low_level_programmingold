9187a9d9b1bc3bfd7d13ab8f74d97e1a731b8ae2
jopinta <tatinfa@yahoo.com>
2021-06-28 12:23:55 -0700

    swap with temp, use space

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * swap_int - check the code for Holberton School students.
+ *
+ *
+ *@a: algo son
+ *@b: losparametros
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+
+int t;
+
+t = * b;
+* b = * a;
+* a = t;
+}
