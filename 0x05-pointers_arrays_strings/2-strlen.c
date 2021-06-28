c5bcb371b1033568a6a5027685a30a38268e4300
jopinta <tatinfa@yahoo.com>
2021-06-28 13:06:47 -0700

    len[]

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strlen - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *
+ *@s: ptr w string
+ */
+
+int _strlen(char *s)
+{
+
+int len;
+
+for (len = 1; s[len] != '\0'; len++)
+{
+}
+return (len);
+}
