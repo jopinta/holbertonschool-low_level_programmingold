eebc18190e942fcaceb2220f1a4c7d92f25f179f
jopinta <tatinfa@yahoo.com>
2021-06-28 17:29:36 -0700

    temp se necesita y b++i--

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rev_string - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *@s: string
+*/
+
+
+void rev_string(char *s)
+{
+
+int i, b, x;
+
+
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+
+i = i - 1; 
+
+for (b = 0; b <= i; b++, i--)
+{
+x = s[b];
+s[b] = s [i];
+s[i] = x;
+}
+}
