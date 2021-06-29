52f1b9b0707b17c3e032b49536c0ff441e7b7c71
jopinta <tatinfa@yahoo.com>
2021-06-28 18:06:57 -0700

    %2 newline

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts2 - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *@str: dtring
+*/
+
+void puts2(char *str)
+{
+
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+
+if ((i % 2) == 0)
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
