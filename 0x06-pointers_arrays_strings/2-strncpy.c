7e39360dd2915bc75abd695637354cd094d4d2a6
jopinta <tatinfa@yahoo.com>
2021-07-02 14:19:08 -0700

    recorrer string igualar a 0,

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,20 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 
-  int i = 0;
+int i = 0;
 
-  while(src[i] < n)
-    {
-      dest[i + 1] = src[i];
-      i++;
-    }
-  return (dest);
+
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+ 
+ 
+return (dest);
 }
