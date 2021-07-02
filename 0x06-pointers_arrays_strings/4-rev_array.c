9de9bfc2cd7995209fe3cdc0354cf6457e29fb06
jopinta <tatinfa@yahoo.com>
2021-07-01 17:12:31 -0700

    with tempo and --

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,15 +11,15 @@
 
 void reverse_array(int *a, int n)
 {
-  int i;
-  int temp;
-    for (i = 0; i < n; i++)
-      {
-	n--;	
-       temp = a[i];
-      	a[i] = a[n];
-	a[n] = temp;
-	
-      }
+int i;
+int temp;
+for (i = 0; i < n; i++)
+{
+n--;	
+temp = a[i];
+a[i] = a[n];
+a[n] = temp;
+
+}
 }
 
