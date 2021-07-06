0a6e250f5cd840978bc994ca2c071540f87f03f0
jopinta <tatinfa@yahoo.com>
2021-07-05 18:49:56 -0700

    set 2 to

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * set_string - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *@s: sterre
+ *@to: ptr 
+*/
+
+void set_string(char **s, char *to)
+
+{
+
+*s = to;
+}
