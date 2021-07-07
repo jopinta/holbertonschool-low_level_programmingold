54a598d84af9445d795f6c320dad78a305eec17a
jopinta <tatinfa@yahoo.com>
2021-07-07 07:24:36 -0700

    in class

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *
+ *@s:string
+*/
+
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		_putchar (*s);
+			_puts_recursion(s + 1);
+	}
+}
