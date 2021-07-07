637a792fa14676fea29c005f54b6357fb47e56e7
jopinta <tatinfa@yahoo.com>
2021-07-07 08:58:34 -0700

    para que impima alreves poner el s +1 antesdel _putchar s

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ *
+ * Return: Always 0.
+ *
+ *@s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+
+		return;
+	{
+		_print_rev_recursion(s + 1);
+		 _putchar (*s);
+	}
+}
