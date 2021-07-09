021cc4fc651856842a05ff526529d054ebc37dec
jopinta <tatinfa@yahoo.com>
2021-07-09 16:53:03 -0700

    usando atoi

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *
+ *
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int suma;
+	int res = 0;
+
+		for (suma = 1; suma < argc -1; suma++)
+		{
+			if (atoi(argv[suma]) == 0 && argv[suma] == 48)
+			{
+			printf("Error\n");
+			return (1);
+			}
+			else
+			{
+				res = atoi(res + argv[suma]);
+			}
+		}
+		printf("%d\n", res);
+		return (0);
+	}
