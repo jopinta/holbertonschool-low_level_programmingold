137532c9b7f8580d77c3d27e536436ec7c39e14e
jopinta <tatinfa@yahoo.com>
2021-07-09 16:24:54 -0700

    all args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - entryi pont
+ *
+ * Return: 0
+ *@argc: contador
+ *@argv: vector
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+	printf("%s\n", argv[i]);
+
+return (0);
+}
