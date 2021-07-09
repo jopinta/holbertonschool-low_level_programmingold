cd50666b297407daefa4a0301edaa589b72cd8bf
jopinta <tatinfa@yahoo.com>
2021-07-09 11:11:42 -0700

    anular el q se nousa

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/** main - funtion entry point
+ *
+ *return - always 0
+ *@argc: cantidad
+ *@argv: values
+ */
+
+
+int main(int argc, char *argv[])
+
+{
+	printf("%s\n", argv[0]);
+	(void) argc;
+	return (0);
+	}
