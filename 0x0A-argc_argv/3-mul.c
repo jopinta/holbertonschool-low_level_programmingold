4d8b97bd0f7190441bb667162649731be64e62c6
jopinta <tatinfa@yahoo.com>
2021-07-09 16:43:39 -0700

    return adedd

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *
+ * Return: o y 1
+ *@argc: count
+ *@argv: vector
+ *
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int val;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+
+val = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", val);
+return (1);
+}
