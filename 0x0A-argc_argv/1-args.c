325cf257ecfe69c7ad081139e8b7c23ec13c4318
jopinta <tatinfa@yahoo.com>
2021-07-09 16:21:20 -0700

    passing arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - entri point
+ *
+ * Return: 0
+ *
+ *@argc: count
+ *@argv: vector
+ */
+
+int main(int argc,  __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
