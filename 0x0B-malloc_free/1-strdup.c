bdfb035e2d55f98d2c439132748e756fee5397bc
jopinta <tatinfa@yahoo.com>
2021-07-13 15:06:53 -0700

    len cpy str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+/**
+ *_strdup - check the code for Holberton School students.
+ *
+ *@str: returns a pointer to the duplicated string
+ *
+ * Return: Always 0.
+ */
+
+
+char *_strdup(char *str)
+{
+
+	char *p = NULL;
+	int i = 0;
+	int len = 0;
+
+	for (len = 0; str[len] != '\0'; len++)
+	p = malloc(sizeof(char) * len);
+	if (p)
+	{
+		for (i = 0; i < len; i++)
+			p[i] = str[i];
+	}
+	return (p);
+}
