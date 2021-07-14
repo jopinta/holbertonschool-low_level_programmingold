fb449b831aef0a68892d36dcfe22a9d9d1ee8e13
jopinta <tatinfa@yahoo.com>
2021-07-14 13:13:04 -0700

    case: str_concat(, Hello);str_concat(Hello, NULL); str_concat(NULL, Hello); str_concat(NULL, NULL);fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,6 +17,10 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	int l1, l2;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (l1 = 0; s1[l1]; l1++)
 		for (l2 = 0; s2[l2]; l2++)
