e6c56275bd964c0191aecf94308824092b6a420f
jopinta <tatinfa@yahoo.com>
2021-07-14 13:04:31 -0700

    suspect codeindent with {} line 25

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *
+ *@s1: string one
+ *@s2: sting two
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p = NULL;
+	int i = 0;
+	int l1, l2;
+
+
+	for (l1 = 0; s1[l1]; l1++)
+		for (l2 = 0; s2[l2]; l2++)
+			p = malloc(sizeof(char) * (l1 + l2 + 1));
+
+	if (!p)
+
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		p[i] = s1[i];
+	for (i = 0; s2[i] != '\0'; i++)
+		p[l1 + i] = s2[i];
+	p[l1 + l2] = '\0';
+return (p);
+		}
