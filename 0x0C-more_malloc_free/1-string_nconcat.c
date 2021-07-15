87542e4c0be97cdae7d24c04bc4db22b75baaca7
jopinta <tatinfa@yahoo.com>
2021-07-15 13:49:04 -0700

    prueba

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *@s1: string one
+ *@s2: stringggtwo
+ *@n: n bytes asked
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	char *britney = NULL;
+	 unsigned int i;
+	 int j;
+
+
+	for (i = 0; s2[i] != '\0'; i++)
+		if (n >= i)
+			n = n + 1;
+
+	for (j = 0; s1[j] != '\0'; j++)
+
+		britney =(char *)malloc(sizeof(char) * j + n + 1);
+	if (!britney)
+	{
+		return (NULL);
+
+}
+	return (britney);
+}
