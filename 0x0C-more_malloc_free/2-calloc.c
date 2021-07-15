9fce9c9e6ec91e7b89d35ab9bbda8b335e1fb133
jopinta <tatinfa@yahoo.com>
2021-07-15 16:31:59 -0700

    indentado funca

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *lolita;
 	unsigned int i;
 
-	if (nmemb == '\0' || size == '\0')
+	if (!nmemb || !size)
 		return (NULL);
 	lolita = malloc(nmemb * size);
 	if (!lolita)
