2b85a262138347379d7b94dfdc4f91ce09031d63
jopinta <tatinfa@yahoo.com>
2021-07-15 16:19:35 -0700

    calloc nmemb * size

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == '\0' && size == '\0')
 		return (NULL);
 
-	lolita = malloc(sizeof(nmemb * size));
+	lolita = malloc(nmemb * size);
 		if (!lolita)
 			return (NULL);
 			for (i = 0; i < (nmemb * size); i++)
