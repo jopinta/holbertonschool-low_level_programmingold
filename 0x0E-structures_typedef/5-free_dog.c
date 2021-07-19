0cac3ba9d1e1a932d6652abb6aa5a22c5dc45e5f
jopinta <tatinfa@yahoo.com>
2021-07-19 16:57:46 -0700

    free name and owner. and age?

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - who let the dogs out
+ *@d: structure to breakfeee
+ *
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d->age)
+	}
+	free(d);
+}
