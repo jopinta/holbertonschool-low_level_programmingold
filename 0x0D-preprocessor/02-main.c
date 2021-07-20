b6daa0d5b57f7f7e1189d5bcf503ee33eb23b3d4
jopinta <tatinfa@yahoo.com>
2021-07-19 17:33:03 -0700

    02

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point tothe programm
+ *
+ *@void: does not recieve parameteres
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n)", __FILE__);
+	return (0);
+}
