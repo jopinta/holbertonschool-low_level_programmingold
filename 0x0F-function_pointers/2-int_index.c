f7bf2bfdf1b18906765ae0068f512d7107f74815
jopinta <tatinfa@yahoo.com>
2021-07-21 16:07:52 -0700

    null is not an int line 20

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - check if a number is equal to 98
+ *@array: element to print in cmp fun
+ *@size: of array
+ *@cmp: name of function
+ * Return: to index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
