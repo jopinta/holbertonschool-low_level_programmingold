06c22519c196644fc0161eb9b221e1235fd940be
jopinta <tatinfa@yahoo.com>
2021-07-29 16:41:51 -0700

    hstrhlen

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+/**
+ *print_list - printea todo
+ *
+ *@h: lista
+ *
+ *
+ *Return: contador
+ *
+ *
+ */
+
+size_t print_list(const list_t *h)
+{
+
+size_t i = 0;
+
+while (h)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+printf("[%d %s,\n", h->len, h->str);
+i++;
+h = h->next;
+}
+
+return (i);
+}
