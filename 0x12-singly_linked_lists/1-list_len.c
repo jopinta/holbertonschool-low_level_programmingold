36f2991ec410d9cdedb9e06c06c0171343e55a9b
jopinta <tatinfa@yahoo.com>
2021-07-29 16:12:24 -0700

    include prototype en .h

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *list_len - largo de l lista
+ *
+ *@h: lista+
+ *
+ * Return - number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nodo = 0;
+
+	while (h)
+	{
+		nodo++;
+		h = h->next;
+	}
+	return (nodo);
+}
