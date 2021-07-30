a52b59600b400074b6e2673dcfa4e2ec133638dc
jopinta <tatinfa@yahoo.com>
2021-07-30 16:33:12 -0700

    helped pld

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+
+/**
+ *add_node_end - function that creates and add a node at he end
+ *
+ *@head: pontier to pinter head list_t
+ *@str: str needs to be duplicated
+ *
+ *Return: he address of the new element
+ *
+0;10;1c */
+
+
+
+list_t *add_node_end(list_t **head, const char *str)
+
+{
+	list_t *nodo;
+	list_t *prev;
+	int i = 0;
+
+	if (!(head && str))
+	return (NULL);
+
+	    nodo = malloc(sizeof(list_t));
+
+if (!nodo)
+return (NULL);
+nodo->str = strdup(str);
+for (i = 0; str[i]; i++)
+	;
+	    nodo->len = i;
+	    nodo->next = NULL;
+
+if (!*head)
+{
+*head = nodo;
+return (nodo);
+}
+prev = *head;
+while (prev->next)
+{
+prev = prev->next;
+}
+prev->next = nodo;
+return (nodo);
+
+}
