bcf35e3fbc049dbace6929b8fc3a26c6590cf268
jopinta <tatinfa@yahoo.com>
2021-08-02 16:48:11 -0700

    igualar nodend to *head

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ *add_nodeint_end - add node at the end
+ *@head: struct with th elinked list.
+ *@int: int to copy
+ *Return: address of hte new element.
+ */
+
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *nodoend;
+	listint_t *nodo;
+
+	nodo = malloc(sizeof(listint_t));
+
+	if (nodo)
+	{
+		nodo->n = n;
+		nodo->next = NULL;
+
+		if (*head == NULL)
+			{
+				*head = nodo;
+				return (nodo);
+			}
+		nodoend = *head;
+
+		while (nodoend->next != NULL)
+		nodoend = nodoend->next;
+		nodoend->next = nodo;
+		return (nodo);
+	}
+	free(nodo);
+	return (NULL);
+}
