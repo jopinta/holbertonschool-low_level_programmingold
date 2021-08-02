d4b24f14a7da46ab99ac5a53d42fbca5c1f41aee
jopinta <tatinfa@yahoo.com>
2021-08-02 15:47:55 -0700

    diego explaines dif b c or i

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add node.
+ * @head: pointer to the struct.
+ * @n: element to add on the new node
+ *
+ * Return: nodo numbers
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *nodo
+
+		nodo = malloc(sizeof(listint_t));
+	if (nodo)
+	{
+		nodo->n = n;
+		nodo->next = *head;
+		*head = nodo;
+		return (nodo);
+}
+	free(nodo);
+	return (NULL);
+}
