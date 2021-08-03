b2a291a4777bad985f3d02d1a2e600b07f332bc6
jopinta <tatinfa@yahoo.com>
2021-08-03 13:06:53 -0700

    gotta do head == null?

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-if (!head)
-return (NULL);
-
 for (i = 0; i < index; i++)
-	{
-		head = head->next;
-	}
+{
+	head = head->next;
+}
 return (head);
 }
