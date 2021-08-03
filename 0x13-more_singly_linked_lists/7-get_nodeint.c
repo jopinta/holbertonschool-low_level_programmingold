80d01ab62e79943616e47c8cfc975c69eeb0af3c
jopinta <tatinfa@yahoo.com>
2021-08-03 13:01:02 -0700

    geekforgeeks solution with variable the sum

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *get_nodeint_at_index - returns the xth node
+ *@head: linked list
+ *@index: inex's node
+ *Return: nth node fron the list
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+if (!head)
+return (NULL);
+
+for (i = 0; i < index; i++)
+	{
+		head = head->next;
+	}
+return (head);
+}
