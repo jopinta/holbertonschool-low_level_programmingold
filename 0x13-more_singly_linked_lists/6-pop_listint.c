392d14f1c3785b23b57f7a322f41fd82a7ef6857
jopinta <tatinfa@yahoo.com>
2021-08-04 06:44:10 -0700

    pld helped

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that delete the head node
+ * @head: pointer to the pointer to the linked list
+ *
+ * Return: the head node data
+ */
+
+int pop_listint(listint_t **head)
+{
+
+	listint_t *node;
+	int i;
+
+	if (!*head)
+		return (0);
+	i = (*head)->n;
+	node = *head;
+	*head = (*head)->next;
+	free(node);
+	return (i);
+}
