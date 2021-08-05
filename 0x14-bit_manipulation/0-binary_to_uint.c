7761fc7df1538a49b813a8021e7fdf0b93256fba
jopinta <tatinfa@yahoo.com>
2021-08-05 12:36:03 -0700

    convertion with <<=

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,14 +16,17 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 	unsigned int converted;
 
-        if (b == NULL)
-		return (0);
+
 
 	for (i = 0; i < b; i++)
 		;
 	if  (b[i] != '0' || b[i] != '1')
 		return (NULL);
-	else
-	{ return (1)
+else
+	{
+		converted <<= 1;
+		if (b[i] == '1')return (1)
+					num += 1;
 	}
+	return (converted)
 }
