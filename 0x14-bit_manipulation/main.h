80e64ef9810c83dbfaf6af9478cd1e00f976d9a1
jopinta <tatinfa@yahoo.com>
2021-08-06 10:59:06 -0700

    pld help

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,9 @@
 #ifndef MAINC
 #define MAINC
 
+#include <stdio.h>
+#include <string.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
