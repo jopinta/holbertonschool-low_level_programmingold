3ed5cbffb8ea1091eb87e1b5816497734ccdbbd0
jopinta <tatinfa@yahoo.com>
2021-08-09 16:13:21 -0700

    O_TRUNC

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	fd = open(filename, O_CREAT | O_RDWR | 0_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if  (fd == -1)
 		return (-1);
 
