6091759c652da12a81c063e8ad593ea55667b571
jopinta <tatinfa@yahoo.com>
2021-08-09 16:02:58 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,5 @@
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <stddef.h>
 #include "main.h"
 #include <stdio.h>
@@ -8,7 +10,7 @@
 
 
 /**
- * create_file - Entry point
+ * create_file - function that creates a file.
  * @filename: Pointer to file
  * @text_content: Content to insert in the file
  *
@@ -19,26 +21,26 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd = 0;
+	int cont = 0;
 
 	if (!filename)
-	{
 		return (-1);
-	}
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	if (!text_content)
+		text_content = "";
 
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
 	if  (fd == -1)
-	{
 		return (-1);
-	}
-	while (*text_content)
+
+	while (text_content[cont] != '\0')
 	{
-		if (write(fd, text_content, 1) == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-		text_content++;
+		cont++;
+	}
+	if ((write(fd, text_content, cont)) == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 	close(fd);
-	return (0);
+	return (1);
 }
