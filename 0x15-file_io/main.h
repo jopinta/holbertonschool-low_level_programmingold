e918827476d3a695116044729eb71fbee14ed8fb
jopinta <tatinfa@yahoo.com>
2021-08-09 15:45:14 -0700

    updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "main.h"
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stddef.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-
+int _putchar (char c);
 
 #endif
