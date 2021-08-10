04b4d4bf1fa9b4d717ee43a1919c00c637cc4533
jopinta <tatinfa@yahoo.com>
2021-08-10 15:18:52 -0700

    estaba pasando mal el valor sssize_t

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@
 int main(int argc, char **argv)
 {
 
-	int from, to, i, j;
+	int file_from, file_to, i, j;
 	char buff[1024];
 	ssize_t cont;
 
@@ -26,27 +26,27 @@ int main(int argc, char **argv)
 			exit(97);
 		}
 
-	from = open(argv[1], O_RDONLY);
+	file_from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 }
 
-to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (to == -1)
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (file_to == -1)
 {
-	i = close(from);
+	i = close(file_from);
 		if (i == -1)
 		{
-			dprintf(2, "Error: Can't close fd %i", from);
+			dprintf(2, "Error: Can't close fd %i", file_from);
 			exit(100);
 		}
 		exit(98);
 }
-	while ((cont = read(from, buff, sizeof(buff))) != 0)
+	while ((cont = read(file_from, buff, sizeof(buff))) != 0)
 	{
-		j = write(to, buff, cont);
+		j = write(file_to, buff, cont);
 		if (j == -1)
 		{
 			dprintf(2, "Error: Can't write to %s", argv[2]);
