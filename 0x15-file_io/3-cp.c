24d111cf513b70f3f84e9137d7e4d5abe1d86b5d
jopinta <tatinfa@yahoo.com>
2021-08-10 15:50:42 -0700

    indented code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,13 +5,8 @@
  *@argc: name of the program
  *@argv: argument vector
  *
- *Return: bytes readed
- *
- *
- *
- *
+ *Return: bytes read
  */
-
 
 int main(int argc, char **argv)
 {
@@ -25,31 +20,29 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 			exit(97);
 		}
-
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-}
-
-file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (file_to == -1)
-{
-	i = close(file_from);
-		if (i == -1)
+		file_from = open(argv[1], O_RDONLY);
+		if (file_from == -1)
 		{
-dprintf(STDERR_FILENO, "Error: Can't close fd %i", file_from);
-			exit(100);
-		}
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-}
-	while ((cont = read(file_from, buff, sizeof(buff))) != 0)
+		}
+		file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+		if (file_to == -1)
+		{
+			i = close(file_from);
+	if (i == -1)
 	{
-		j = write(file_to, buff, cont);
+		dprintf(2, "Error: Can't close fd %i", file_from);
+		exit(100);
+	}
+	exit(98);
+		}
+		while ((cont = read(file_from, buff, sizeof(buff))) != 0)
+		{
+			j = write(file_to, buff, cont);
 		if (j == -1)
 		{
-dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			dprintf(2, "Error: Can't write to %s", argv[2]);
 			exit(99);
 		}
 	}
