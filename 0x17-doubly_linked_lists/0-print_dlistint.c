c7a0e0066605ad028ce24652fda76498c973400f
jopinta <tatinfa@yahoo.com>
2021-09-16 09:16:10 -0700

    back to C

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - print doubly list
+ *
+ *
+ *
+ *
+ */
+
+size_t print_dlistint(const dlistint_t *h);
+{
+	int nodes = 0;
+
+
+	if (h is NULL)
+		return (nodes);
+
+	while (h != NULL)
+	{
+		print("%d\n", h->n)
