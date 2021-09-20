79b2557a0e2bb7acfd76762f92963ec14f46b6d4
jopinta <tatinfa@yahoo.com>
2021-09-20 12:49:03 -0700

    completando 4 5 69

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strcpy - ul
+ * @dest: des
+ * @src: src
+ *
+ * Description: Copy a string from source to destination
+ * Return: string destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0, len = _strlen(src);
+
+for (i = 0 ; i <= len ; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
